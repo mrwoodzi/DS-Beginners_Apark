8977bd51ed51bfb39361f4ce613adeefe8f1f977
Michael Woods <mrwoodzi@protonmail.com>
2021-01-08 15:58:36 -0600

    DS-Beginners_APark

---
+++ ex37.py
@@ -0,0 +1,23 @@
+i = 10
+j = 15
+### HOLY CRAP THERE WERE A LOT OF PROBLEMS IN THE BOOK AND IT WOULDN'T RUN
+if( i == j ):
+    print("\t i is equal to j")
+else:
+    print("\t i is not equal to j")
+
+if(i != j):
+    print("\t i is not equal to j")
+else:
+    print("\t i is equal to j")
+
+if( i > j):
+    print("\t i is greater than j")
+else:
+    print("\t i is not greater than j")
+
+if( i < j):
+    print("\t i is less than j")
+else:
+    print("\t I is not less than j")
+#But I did figure it out and it ruuuuns!
