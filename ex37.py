689863ba2082483955771b19e17ac2df7991ed91
Michael Woods <mrwoodzi@protonmail.com>
2021-01-09 09:18:59 -0600

    DS-Beginners_APark

--- ex37.py
+++ ex37.py
@@ -6,18 +6,28 @@ if( i == j ):
 else:
     print("\t i is not equal to j")
 
-if(i != j):
+if( i != j ):
     print("\t i is not equal to j")
 else:
     print("\t i is equal to j")
 
-if( i > j):
+if( i > j ):
     print("\t i is greater than j")
 else:
     print("\t i is not greater than j")
 
-if( i < j):
+if( i < j ):
     print("\t i is less than j")
 else:
-    print("\t I is not less than j")
+    print("\t i is not less than j")
 #But I did figure it out and it ruuuuns!
+
+if( i >= j ):
+    print("\t i is greater than or equal to j")
+else:
+    print("\t i is neither greater than nor equal to j")
+
+if( i <= j ):
+    print("\t i is less than or equal to j")
+else:
+    print("\t i is neither less than nor equal to j")
