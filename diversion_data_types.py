4e54095a778a0de70a5f1ea8979829e07b9e6b24
Michael Woods <mrwoodzi@protonmail.com>
2021-01-12 11:35:54 -0600

    DS-Beginners_APark

---
+++ diversion_data_types.py
@@ -0,0 +1,33 @@
+#these examples were from progamiz.com but I added my own personalization
+s = 'Hello World!'
+print("\n\t\tTuples!")
+print("\tTuples exist inside ( , , , )")                          # Tuples
+
+print("\tHello World!")
+
+print("\ts[4] = ", s[4])
+
+print("\ts[6:11] = ", s[6:11])
+                                                 # Sets
+a = {5, 2, 3, 1, 4}
+
+print("\n\t\tSets!")
+print("\tSets exist inside { , , ,}")
+print("\ta = ", a)
+
+print("\t",type(a))#prints data type of variable "a"
+                                               # String
+print("\n\t\tStrings!")
+print("\tStrings are made with ' or \" ")
+my_string = '\tHello'
+print(my_string)
+
+my_string = "\tHello"
+print(my_string)
+
+my_string = '''\tHello'''
+print(my_string)
+
+my_string = """\tHello, welcome to
+\tthe world of Python"""
+print(my_string)
